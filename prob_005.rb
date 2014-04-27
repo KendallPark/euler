3deda7b6565ced79bcce223482678cf9db335936
Kendall Park <kendall.park@cph.org>
2014-04-26 23:09:37 -0500

    started prob 5

---
+++ prob_005.rb
@@ -0,0 +1,11 @@
+# 2520 is the smallest number that can be divided by each of the 
+# numbers from 1 to 10 without any remainder.
+
+# What is the smallest positive number that is evenly divisible 
+# by all of the numbers from 1 to 20?
+
+# brainstorm...
+
+# Find all the prime numbers between 1 to n and multiply
+
+# I think I'm gonna need a seive
